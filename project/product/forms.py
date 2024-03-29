ff4a5d88c3145a7fb81329ec0c053d27cd30302b
Acutapugione <acuta.pugione@gmail.com>
2024-03-29 20:21:33 +0200

    ...

--- project/product/forms.py
+++ project/product/forms.py
@@ -1,3 +1,24 @@
-# TODO: 2 Форми, Створення товару, Оновлення товару
+from django.shortcuts import render, redirect
+from .models import Product
+from .forms import ProductCreationForm, ProductUpdateForm
 
-# Product.objects
+def create_product(request):
+    if request.method == 'POST':
+        form = ProductCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list_url')
+    else:
+        form = ProductCreationForm()
+    return render(request, 'create_product.html', {'form': form})
+
+def update_product(request, product_id):
+    product = Product.objects.get(id=product_id)
+    if request.method == 'POST':
+        form = ProductUpdateForm(request.POST, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('product_list_url')
+    else:
+        form = ProductUpdateForm(instance=product)
+    return render(request, 'update_product.html', {'form': form})
